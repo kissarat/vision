4b15ebde30bc2fbda458af1d566d2ec56534e7f9
Taras Labiak <kissarat@gmail.com>
2019-12-25 04:45:17 +0200

    rename: window

--- src/show-image.cpp
+++ src/show-image.cpp
@@ -1,7 +1,7 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
-const char* window = "First";
+const char* window = "Show Image";
 
 int main(int argc, char** argv) {
     Mat img = imread(argv[1], -1);
